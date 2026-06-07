fff952e04ef0d39cf72fc2846b0d8c71661e060d
Gennadiy Tsekhmistro <Tsekhmistro.gennadiy@gmail.com>
2026-06-07 23:19:10 +0300

    add src/vite-env.d.ts

---
+++ src/components/App/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import { Votes } from "../../types/votes";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (type: keyof Votes) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  );
+}
